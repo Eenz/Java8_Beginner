f78057b543a3247d7112dbd5765f4f82763ee831
Eenz <titkovmax@gmail.com>
2017-06-06 00:39:25 +0300

    Chapter 1.3

---
+++ src/by/schildt/chapter_1/Example4.java
@@ -0,0 +1,12 @@
+package by.schildt.chapter_1;
+
+public class Example4 {
+    public static void main(String[] args) {
+        double g = 10;      // g - gallons
+        double l;           // l - liters
+
+        l = g * 3.7854;
+
+        System.out.println(g + "  галонам соответсвует " + l + " литров");
+    }
+}
