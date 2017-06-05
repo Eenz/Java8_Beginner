08b99fe9674749c0239cf09fcd86547aa1692c17
Eenz <titkovmax@gmail.com>
2017-06-06 00:32:06 +0300

    Chapter 1.3

---
+++ src/by/schildt/chapter_1/Example3.java
@@ -0,0 +1,20 @@
+package by.schildt.chapter_1;
+
+public class Example3 {
+    public static void main(String[] args) {
+        int var;
+        double x;
+
+        var = 10;
+        x = 10.0;
+
+        System.out.println("начальное значение var: " + var);
+        System.out.println("начальное значение х: " + x);
+
+        var = var / 4;
+        x = x / 4;
+
+        System.out.println("значение var после деления: " + var);
+        System.out.println("значение х после деления: " + x);
+    }
+}
