57a4ba56633b351676be6ce312d144fd57481fd5
Eenz <titkovmax@gmail.com>
2017-06-05 15:30:02 +0300

    Chapter 1.2

--- src/by/schildt/chapter_1/Example2.java
+++ src/by/schildt/chapter_1/Example2.java
@@ -2,8 +2,7 @@ package by.schildt.chapter_1;
 
 public class Example2 {
     public static void main(String[] args) {
-        int var1;
-        int var2;
+        int var1, var2;
         var1 = 1024;
 
         System.out.println("переменная var1 содежит " + var1);
