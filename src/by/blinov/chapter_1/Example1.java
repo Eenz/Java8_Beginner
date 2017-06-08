40e579775797cc5b62cb6a2c28279454fe44aa70
Eenz <titkovmax@gmail.com>
2017-06-08 14:16:00 +0300

    Chapter 1 Example 1,2

--- src/by/blinov/chapter_1/Example1.java
+++ src/by/blinov/chapter_1/Example1.java
@@ -1,21 +1,14 @@
 package by.blinov.chapter_1;
 
-import by.schildt.chapter_1.Example2;
-
-class Example1 {
-    void printSlogan() {
-    System.out.println("Enebling the Informations Age");
-    }
-
+public class Example1 {
     public static void main(String[] args) {
-        printSlogan1("hello");
-        Example1 l1 = new Example1();
-        l1.printSlogan();
-        Example2 l2 = new Example2();
-        Example2 l3 = new Example2();
 
+        SloganAction firstObject = new SloganAction();
+        firstObject.printSlogan();
     }
-    static void printSlogan1(String x) {
-        System.out.println(x);
+}
+class SloganAction{
+    void printSlogan(){
+        System.out.println("enabling the information age");
     }
 }
