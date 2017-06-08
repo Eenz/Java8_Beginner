33f6c9c8898c8148e2a26a2c5379ad2a59007ab2
Eenz <titkovmax@gmail.com>
2017-06-08 16:22:01 +0300

    Chapter 1 Example 7,8

---
+++ src/by/blinov/chapter_1/Example8.java
@@ -0,0 +1,10 @@
+package by.blinov.chapter_1;
+
+public class Example8 {
+    public static void main(String[] args) {
+        Example7 ob = new Example7();
+        ob.diametr = -0.12;
+        ob.setWeight(100);
+        System.out.println(ob.diametr);
+    }
+}
