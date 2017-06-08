d3d6b64c1d6b0136523937752e3d9a2b6170859e
Eenz <titkovmax@gmail.com>
2017-06-08 14:24:31 +0300

    Chapter 1 Example 1,2

--- src/by/blinov/chapter_1/Example1.java
+++ src/by/blinov/chapter_1/Example1.java
@@ -1,14 +1,10 @@
 package by.blinov.chapter_1;
+import by.blinov.chapter_1.Example2;
 
 public class Example1 {
     public static void main(String[] args) {
 
-        SloganAction firstObject = new SloganAction();
+        Example2 firstObject = new Example2();
         firstObject.printSlogan();
-    }
-}
-class SloganAction{
-    void printSlogan(){
-        System.out.println("enabling the information age");
     }
 }
---
+++ src/by/blinov/chapter_1/Example2.java
@@ -0,0 +1,7 @@
+package by.blinov.chapter_1;
+
+class Example2{
+        void printSlogan(){
+            System.out.println("enabling the information age");
+        }
+ }
