c64a04b9f98972c12eea911e3f0431df6b2ba100
Eenz <titkovmax@gmail.com>
2017-06-09 23:42:22 +0300

    Chapter 1 Example 14

---
+++ src/by/blinov/chapter_1/console/RunScanner.java
@@ -0,0 +1,11 @@
+package by.blinov.chapter_1.console;
+import java.util.Scanner;
+public class RunScanner {
+    public static void main(String[] args) {
+        System.out.println("Введите Ваше имя и нажмите <Enter>: ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Привет, " + name);
+        scan.close();
+    }
+}
