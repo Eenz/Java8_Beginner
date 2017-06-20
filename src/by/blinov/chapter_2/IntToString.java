e7c248ffaec0e8d2f8f952b83ce6448085547944
Eenz <titkovmax@gmail.com>
2017-06-20 21:55:30 +0300

    Chapter 2 Int_To_String

--- src/by/blinov/chapter_2/IntToString.java
+++ src/by/blinov/chapter_2/IntToString.java
@@ -3,7 +3,7 @@ package by.blinov.chapter_2;
 public class IntToString {
     public static void main(String[] args) {
         int value = 71;
-        
+
             String arg1 = Integer.toString(value); // good method
             String arg2 = String.valueOf(value);    // good method
             String arg3 = "" + value;   //bad method
